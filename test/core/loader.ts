55790298046e86c58ab610232fe0f34bfc688a2d
WittBulter <nanazuimeng123@gmail.com>
2018-01-17 17:42:39 +0800

    test: add loader test

---
+++ test/core/loader.ts
@@ -0,0 +1,15 @@
+import { expect } from 'chai'
+
+describe('Loader resources test', () => {
+  
+  it('should loaded scripts', () => {})
+  
+  it('should loaded pages', () => {})
+  
+  it('should loaded styles', () => {})
+  
+  it('should loaded images', () => {})
+  
+  it('should inserted script to page when script loaded', () => {})
+  
+})
